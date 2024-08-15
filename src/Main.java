13e7bd05db27675e579c9ecac2991dfa4578d054
ankit-suman-07 <ankitsuman07@gmail.com>
2024-08-16 01:45:46 +0530

    demo class created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import all_codes.First_Demo;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("DSA init");
+        First_Demo first = new First_Demo();
+        first.displayPrint();
     }
 }
---
+++ src/all_codes/First_Demo.java
@@ -0,0 +1,7 @@
+package all_codes;
+
+public class First_Demo {
+    public void displayPrint() {
+        System.out.println("Helloooo");
+    }
+}
