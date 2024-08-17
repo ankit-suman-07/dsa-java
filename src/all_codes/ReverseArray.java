21e4ed7b84e4504f9a7df5c3f511483e44b3a526
ankit-suman-07 <ankitsuman07@gmail.com>
2024-08-17 18:32:31 +0530

    reverse-array elements

---
+++ src/all_codes/ReverseArray.java
@@ -0,0 +1,24 @@
+package all_codes;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5 ,6};
+        System.out.println(Arrays.toString(reverseArr(arr)));
+    }
+
+    static int[] reverseArr(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        return arr;
+    }
+}
