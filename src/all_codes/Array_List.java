9a293dabeb6933b78a3ac3e7b3863ee3c67a2a84
ankit-suman-07 <ankitsuman07@gmail.com>
2024-08-17 18:01:30 +0530

    ArrayList code demo

---
+++ src/all_codes/Array_List.java
@@ -0,0 +1,16 @@
+package all_codes;
+
+import java.util.ArrayList;
+
+public class Array_List {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(34);
+        list.add(24);
+        list.add(12);
+        list.add(9);
+        list.add(234);
+
+        System.out.println(list.get(3));
+    }
+}
